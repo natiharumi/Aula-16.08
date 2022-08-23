29985e1ea9ac03aca84e5c37c369ec127deb2391
natiharumi <natalia.harumi2006@gmail.com>
2022-08-23 08:45:02 -0300

    commit-2

--- js/script.js
+++ js/script.js
@@ -22,4 +22,30 @@ $(document).ready(function() {
         $('#mensagem').empty()
     })
 
+    //2 PARTE
+
+    $('p').hide()
+
+    $('#btn-resumo').click(function(e){
+        e.preventDefault()
+
+        //criar uma variavel para receber o status
+        let status = $(this).attr('data-status')
+
+        $('#btn-resumo').empty()
+
+        if(status == '1'){
+           $('#btn-resumo').append('Esconder resumo')
+           $('#btn-resumo').attr('data-status', '2')
+       }
+        else{
+           $('#btn-resumo').append('Ver resumo')
+           $('#btn-resumo').attr('data-status', '1')
+       }
+        
+
+        $('p').toggle(2000)
+    })
+   
+
 })
